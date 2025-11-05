59c2bb620fe9a16e19b8a0bd320553d6ca81c5fb
ena.imamovic <ena.imamovic@student.um.si>
2025-11-05 21:17:06 +0100

    Added PC-Axis file parsing

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
+import iconv from "iconv-lite";
 import { loadData } from "./xmlStore.js";
 import { exportJson, exportXml } from "./utils/export.js";
+import { createRequire } from "module";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const require = createRequire(import.meta.url);
+global._ = require("underscore");
+const PX = require("./lib/px.cjs");
+
+const PORT = 4000;
+
 let DATA = loadData();
+
+const PX_URL = "	https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/15P1201S.PX";
 
 app.get("/api/animals", (req, res) => {
   const { species, city, neutered, maxFee, region } = req.query;
@@ -71,5 +81,25 @@ app.post("/api/export", (req, res) => {
   });
 });
 
+// livestock endpoint (.px file)
+app.get("/api/livestock", async (req, res) => {
+  try {
+    // download .px file dynamically from URL
+    const response = await fetch(PX_URL);
+    const buffer = await response.arrayBuffer();
+    const pxText = iconv.decode(Buffer.from(buffer), "win1250");
 
-app.listen(4000, () => console.log("--- Backend on http://localhost:4000"));
+    // parse using Px.js
+    const pxInstance = new PX(pxText);
+    const variables = pxInstance.variables();
+    const entries = pxInstance.entries();
+
+    res.json({ metadata: variables, data: entries });
+  } catch (err) {
+    console.error("Error parsing PC-AXIS file:", err);
+    res.status(500).json({ error: "Failed to parse PX data" });
+  }
+});
+
+
+app.listen(PORT, () => console.log("--- Backend on http://localhost:4000"));
